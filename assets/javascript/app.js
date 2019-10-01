97624e26b804afa44b3703316ffa96b7a545c283
Varun Master <varunmaster95@gmail.com>
2019-10-01 14:31:23 -0400

    added timer condition. updated checkAnswer method to check if user answered correctly, incorrectly, or no answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,14 @@
-userAnsArr = [];
-AnsArr = [];
-numCorrect = 0;
+var userAnsArr = [];
+var AnsArr = [];
+var numCorrect = 0;
+var numIncorrect = 0;
+var numUnanswered = 0;
+
+timeout = setTimeout(checkAnswers,120000); //if user doesn't submit in 120 seconds, do it automatically
 
 $(document).ready(function (){
     $(".submit").on("click",function (){
+        clearTimeout(timeout); //if the submit button is clicked then stop the timer
         console.log($("input[name='q1']:checked").val());
         userAnsArr.push($("input[name='q1']:checked").val());
         if(!$(".question").is(':checked')){
@@ -18,6 +23,11 @@ function checkAnswers(userAnsArr) {
     for (var i = 0; i < userAnsArr.length; i++){
         if (userAnsArr[i] === AnsArr[i]) {
             numCorrect += 1;
+        }
+        else if (userAnsArr[i] === "undefined") {
+            numUnanswered += 1;
+        }else {
+            numIncorrect += 1;
         }
     }
     return numCorrect;
