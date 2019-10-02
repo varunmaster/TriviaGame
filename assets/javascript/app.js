df0d4be3eca2b39b29c549deb8f3c9bfcbc596ba
Varun Master <varunmaster95@gmail.com>
2019-10-02 19:52:53 -0400

    added addtl HTML for score. added loop to cycle through each question and put userAns in the proper property. added function to check answers. TODO: CheckAnswers() function not counting properly, implement timer, use show/hide to start/re-start game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,30 +8,30 @@ var trivia = [{
     question: "What is 1+1?",
     options: [0, 1, 2, 3],
     userAns: "",
-    correctAns: 2
+    correctAns: "2"
 }, {
     question: "What is 2+2?",
     options: [10, 11, 4, 3],
     userAns: "",
-    correctAns: 4
+    correctAns: "4"
 }, {
     question: "What is 12+1?",
     options: [10, 11, 42, 13],
     userAns: "",
-    correctAns: 13
+    correctAns: "13"
 }, {
     question: "What is 10+10?",
     options: [10, 11, 20, 30],
     userAns: "",
-    correctAns: 20
+    correctAns: "20"
 }, {
     question: "What is 15+15?",
     options: [40, 10, 20, 30],
     userAns: "",
-    correctAns: 30
-}
-];
+    correctAns: "30"
+}];
 
+//creating and calling a function to display the questions from the trivia objectarray
 displayQuestions(0);
 displayQuestions(1);
 displayQuestions(2);
@@ -39,16 +39,48 @@ displayQuestions(3);
 displayQuestions(4);
 
 function displayQuestions(num) {
-    console.log("trivia question is: ", trivia[num].question);
+    // console.log("trivia question is: ", trivia[num].question);
     $(".question." + num).prepend('<p>' + trivia[num].question + '</p>');
 }
 
-for(var i = 0; i < trivia.length; i++){
-    for(var j = 0; j < trivia[i].options.length; j++)
-        $(".answer."+i).append('<input type="radio" name=question'+i+'/> '+trivia[i].options[j]+' <br>');
+//looping through my trivia object arry and then looping through the options array to display each as a button
+for (var i = 0; i < trivia.length; i++) {
+    for (var j = 0; j < trivia[i].options.length; j++)
+        $(".answer." + i).append('<input type="radio" name=question' + i + ' value='+trivia[i].options[j]+' > ' + trivia[i].options[j] + ' <br>');
 }
 
+function checkAnswers() {
+    for(var i = 0; i < trivia.length; i++){
+        console.log()
+        if(trivia[i].userAns === trivia[i].correctAns) {
+            return numCorrect += 1;
+        }
+        else if(trivia[i].userAns === undefined) {
+            return numUnanswered += 1;
+        } 
+        else {
+            return numIncorrect += 1;
+        }
+    }
+}
 
+$(document).ready(function () {
+    $(".submit").on("click", function () {
+        //looping through the inputs (buttons) and then getting each checked value and putting it in userAns property of trivia
+        for(var i  = 0; i < trivia.length; i++) {
+            trivia[i].userAns = $('input[name="question'+i+'"]:checked').val()
+            // console.log($('input[name="question'+i+'"]:checked').val());
+            // console.log("trivia.userans: ", trivia[i].userAns);
+        }
+        checkAnswers();
+        console.log("correct: ", numCorrect);
+        console.log("incorr: ", numIncorrect);
+        console.log("unans: ", numUnanswered);
+        $(".numCorrect").text(numCorrect);
+        $(".numIncorrect").text(numIncorrect);
+        $(".numUnanswered").text(numUnanswered);
+    }); //click ends here
+}); //doucment ends here
 /*
 
 timeout = setTimeout(checkAnswers,120000); //if user doesn't submit in 120 seconds, do it automatically
